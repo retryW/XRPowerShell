d1d9a0caa84c2b4faf219df709fdd185b7338674
retryW <ROBCAL@health.gov.au>
2019-06-13 10:25:30 +1000

    Added Disconnect-XRPL command

--- XRPowerShell.psm1
+++ XRPowerShell.psm1
@@ -22,4 +22,17 @@ Function Connect-XRPL {
     Export-ModuleMember -Variable $webSocket,$cancelToken
 }
 
+Function Disconnect-XRPL {
+    try {
+        $command = $webSocket.CloseAsync([System.Net.WebSockets.WebSocketCloseStatus]::NormalClosure, "Client disconnect request. Closing web socket.", $cancellationToken)
+        while (!$command.IsCompleted) {
+            Start-Sleep -Milliseconds 100
+        }
+        Write-Host "Disconnected!" -ForegroundColor Yellow
+    } catch {
+        Write-Host "Error!" -ForegroundColor Red
+        Write-Host $_.Exception.Message
+    }
+}
+
 Export-ModuleMember -Function *
